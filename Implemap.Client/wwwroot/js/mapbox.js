b6ea0d9c75d7b2ac128df96ead302c363f3e317e
viktorv9 <38245893+viktorv9@users.noreply.github.com>
2022-04-28 15:58:32 +0200

    Mapbox Map on /map page
    Als je je eigen map/style/data/account wil gebruiken moet je in mapbox.js de style property aanpassen
    
    Todo:
    - interactie
    - dynamisch inladen data

---
+++ Implemap.Client/wwwroot/js/mapbox.js
@@ -0,0 +1,13 @@
+﻿
+// todo: maybe move this somewhere sensible where it still has access to <head>
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidmlrdG9ydngiLCJhIjoiY2wyYWc5aWoyMDU4MDNpc2NpeHRhd3BnbiJ9.0ToPaRrFSK_tM7pZDZR2Ww';
+
+window.loadMapBox = () => {
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/viktorvx/cl28zqp75000015pqm7dffov8', // style URL (also contains data layer)
+        center: [4.90841, 52.3636], // starting position [lng, lat]
+        zoom: 11 // starting zoom
+    });
+}
