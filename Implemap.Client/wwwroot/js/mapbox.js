df3455391b6959d829834c07cb7a0aad27761c45
viktorv9 <38245893+viktorv9@users.noreply.github.com>
2022-05-11 16:38:01 +0200

    generates (hardcoded) clickable markers

--- Implemap.Client/wwwroot/js/mapbox.js
+++ Implemap.Client/wwwroot/js/mapbox.js
@@ -3,11 +3,32 @@
 
 mapboxgl.accessToken = 'pk.eyJ1IjoidmlrdG9ydngiLCJhIjoiY2wyYWc5aWoyMDU4MDNpc2NpeHRhd3BnbiJ9.0ToPaRrFSK_tM7pZDZR2Ww';
 
+var listings = [{
+    "id": 2818,
+    "latitude": 52.36435,
+    "longitude": 4.94358,
+    "name": "Quiet Garden View Room & Super Fast WiFi"
+}, {
+    "id": 1149694,
+    "latitude": 52.38231,
+    "longitude": 4.84866,
+    "name": "Cosy city apartment"
+}];
+
 window.loadMapBox = () => {
     const map = new mapboxgl.Map({
         container: 'map', // container ID
         style: 'mapbox://styles/viktorvx/cl28zqp75000015pqm7dffov8', // style URL (also contains data layer)
         center: [4.90841, 52.3636], // starting position [lng, lat]
         zoom: 11 // starting zoom
+    });
+
+    listings.forEach(e => {
+        console.log(e);
+        const marker = new mapboxgl.Marker().setLngLat([e.longitude, e.latitude]).addTo(map);
+        // use getElement to add an event
+        marker.getElement().addEventListener('click', () => {
+            alert(e.id + ": " + e.name);
+        });
     });
 }
