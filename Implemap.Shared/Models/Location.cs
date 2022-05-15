b30e000b689432b9d3c500407021dc0d8012ffa1
Thijmen Maus <thijmen@maus.nu>
2022-05-15 20:42:09 +0200

    Remove `Implemap`  folder because of redundancy

--- Implemap.Shared/Models/Location.cs
+++
@@ -1,9 +0,0 @@
-namespace Implemap.Shared.Models;
-
-public class Location
-{
-    public int Id { get; set; }
-    public double Latitude { get; set; }
-    public double Longitude { get; set; }
-    public string Name { get; set; }
-}
